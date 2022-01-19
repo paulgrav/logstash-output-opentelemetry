0cec0e1d7fdf0f8b38d47aed52f40ae0caa8b9c3
Paul Grave <paul@stomer.net>
2022-01-19 08:22:26 +0000

    added: ability to configure fields

--- src/main/java/net/stomer/Opentelemetry.java
+++ src/main/java/net/stomer/Opentelemetry.java
@@ -11,6 +11,7 @@ import io.opentelemetry.exporter.otlp.http.logs.OtlpHttpLogExporter;
 import io.opentelemetry.exporter.otlp.logs.OtlpGrpcLogExporter;
 import io.opentelemetry.sdk.logs.LogProcessor;
 import io.opentelemetry.sdk.logs.SdkLogEmitterProvider;
+import io.opentelemetry.sdk.logs.data.Severity;
 import io.opentelemetry.sdk.logs.export.BatchLogProcessor;
 import io.opentelemetry.sdk.logs.export.LogExporter;
 import io.opentelemetry.sdk.resources.Resource;
@@ -34,9 +35,20 @@ public class Opentelemetry implements Output {
             PluginConfigSpec.stringSetting("protocol", "", false, false);
     public static final PluginConfigSpec<String> COMPRESSION_CONFIG =
             PluginConfigSpec.stringSetting("compression", "none");
+
+    public static final PluginConfigSpec<Map<String, Object>>  ATTRIBUTES_CONFIG = PluginConfigSpec.hashSetting("attributes",null, false, false);
+    public static final PluginConfigSpec<Map<String, Object>> RESOURCE_CONFIG = PluginConfigSpec.hashSetting("resource", null, false, false);
+    public static final PluginConfigSpec<String> TRACE_ID_CONFIG = PluginConfigSpec.stringSetting("trace_id", null, false, false);
+    public static final PluginConfigSpec<String> SPAN_ID_CONFIG = PluginConfigSpec.stringSetting("span_id", null, false, false);
+    public static final PluginConfigSpec<String> SEVERITY_TEXT_CONFIG = PluginConfigSpec.stringSetting("severity_text", null, false, false);
+    public static final PluginConfigSpec<String> TRACE_FLAGS_CONFIG = PluginConfigSpec.stringSetting("trace_flags", null, false, false);
+    public static final PluginConfigSpec<String> NAME_CONFIG = PluginConfigSpec.stringSetting("name", null, false, false);
+    public static final PluginConfigSpec<String> BODY_CONFIG = PluginConfigSpec.stringSetting("body", "%{message}", false, false);
+
     private enum VALID_PROTOCOL_OPTIONS {grpc, http};
     private final String id;
     private final PrintStream printer;
+    private final Configuration configuration;
     private final CountDownLatch done = new CountDownLatch(1);
     private volatile boolean stopped = false;
     private final SdkLogEmitterProvider sdkLogEmitterProvider;
@@ -46,52 +58,68 @@ public class Opentelemetry implements Output {
         this(id, configuration, context, System.out);
     }
 
-    private io.opentelemetry.context.Context getContextForEvent(Event event) {
-        TraceState ts = TraceState.getDefault();
-        TraceFlags tf = TraceFlags.getDefault();
-        SpanContext sp = null;
+    private String extractFieldForEvent(Event event, String field) {
+        if(field == null) return null;
+
+        String output = null;
         try {
-            sp = SpanContext.create(
-                    event.sprintf("%{trace.id}"),
-                    event.sprintf("%{span.id}"),
-                    tf,
-                    ts
-                );
+            output = event.sprintf(field);
+            if(output.equals(field)) return null;
         } catch (IOException e) {
             printer.println("IO Exception");
         }
+        return output;
+    }
+
+    private io.opentelemetry.context.Context getContextForEvent(Event event) {
+        TraceState ts = TraceState.getDefault();
+        TraceFlags tf = TraceFlags.fromByte(Byte.parseByte(extractFieldForEvent(event, configuration.get(TRACE_FLAGS_CONFIG))));
+        String traceId = extractFieldForEvent(event, configuration.get(TRACE_ID_CONFIG));
+        String spanId = extractFieldForEvent(event, configuration.get(SPAN_ID_CONFIG));
+
+        SpanContext sp = SpanContext.create(traceId, spanId, tf, ts);
         return io.opentelemetry.context.Context.root().with(Span.wrap(sp));
     }
 
-    private Attributes getAttributesForEvent(Event event) {
+    private Attributes getDefaultAttributes(Event event) {
         Map<String, Object> eventData = event.getData();
         AttributesBuilder a = Attributes.builder();
         for (String key : eventData.keySet()) {
-            if (key.equals("@timestamp") || key.equals("message")) continue;
+            if (key.equals("@timestamp")) continue;
 
             a.put(key, (eventData.get(key)).toString());
         }
         return a.build();
     }
 
-    private void emitLog(Event event) {
-        String message = null;
-        String logLevel = null;
-        Attributes attributes = null;
-        io.opentelemetry.context.Context c = null;
-        try {
-            c = getContextForEvent(event);
-            message = event.sprintf("%{message}");
-            logLevel = event.sprintf("%{log.level}");
-            attributes = getAttributesForEvent(event);
-        } catch (IOException e) {
-            printer.println("IO Exception");
+    private Attributes getAttributesForConfigAndEvent(Map<String, Object> config, Event event) {
+        AttributesBuilder a = Attributes.builder();
+        for(String key: config.keySet()) {
+            String fieldValue = extractFieldForEvent(event, (String)config.get(key));
+            a.put(key, fieldValue);
         }
+        return a.build();
+    }
+
+    private Attributes getAttributesForEvent(Event event) {
+        Map<String, Object> attributeConfig = configuration.get(ATTRIBUTES_CONFIG);
+        if(attributeConfig == null) return getDefaultAttributes(event);
+
+        return getAttributesForConfigAndEvent(attributeConfig, event);
+    }
+
+    private void emitLog(Event event) {
+        io.opentelemetry.context.Context c = getContextForEvent(event);
+        String body = extractFieldForEvent(event, configuration.get(BODY_CONFIG));
+        String severityText = extractFieldForEvent(event, configuration.get(SEVERITY_TEXT_CONFIG));
+        String name = extractFieldForEvent(event, configuration.get(NAME_CONFIG));
+        Attributes attributes = getAttributesForEvent(event);
 
         sdkLogEmitterProvider.get("logstash-output-opentelemetry").logBuilder()
                 .setEpoch(event.getEventTimestamp())
-                .setSeverityText(logLevel)
-                .setBody(message)
+                .setSeverityText(severityText)
+                .setName(name)
+                .setBody(body)
                 .setAttributes(attributes)
                 .setContext(c)
                 .emit();
@@ -135,19 +163,30 @@ public class Opentelemetry implements Output {
                 .build();
     }
 
-    Opentelemetry(final String id, final Configuration config, final Context context, OutputStream targetStream) {
-        // constructors should validate configuration options
-        this.id = id;
+    Attributes getResourceAttributes() {
 
-        printer = new PrintStream(targetStream);
-        Attributes resourceAttributes;
-        resourceAttributes = Attributes.builder()
+        AttributesBuilder attributesBuilder = Attributes.builder()
                 .put("telemetry.sdk.name","logstash-output-opentelemetry")
                 .put("telemetry.sdk.language", "java")
                 .put("telemetry.sdk.version","0.0.1")
-                .put("agent.id", id)
-                .build();
-        Resource r = Resource.create(resourceAttributes);
+                .put("agent.id", id);
+        Map<String, Object> resourceConfig = configuration.get(RESOURCE_CONFIG);
+
+        if(resourceConfig != null) {
+            for(String key: resourceConfig.keySet()) {
+                attributesBuilder.put(key, (String)resourceConfig.get(key));
+            }
+        }
+        return attributesBuilder.build();
+    }
+
+    Opentelemetry(final String id, final Configuration config, final Context context, OutputStream targetStream) {
+        // constructors should validate configuration options
+        this.id = id;
+        this.configuration = config;
+
+        printer = new PrintStream(targetStream);
+        Resource r = Resource.create(getResourceAttributes());
         LogExporter exporter = logExporterForConfig(config);
 
         LogProcessor batchProcessor = BatchLogProcessor.builder(exporter).build();
@@ -180,7 +219,20 @@ public class Opentelemetry implements Output {
 
     @Override
     public Collection<PluginConfigSpec<?>> configSchema() {
-        return PluginHelper.commonOutputSettings(Arrays.asList(ENDPOINT_TYPE_CONFIG,ENDPOINT_CONFIG,PROTOCOL_CONFIG,COMPRESSION_CONFIG));
+        return PluginHelper.commonOutputSettings(Arrays.asList(
+                ENDPOINT_TYPE_CONFIG,
+                ENDPOINT_CONFIG,
+                PROTOCOL_CONFIG,
+                COMPRESSION_CONFIG,
+                BODY_CONFIG,
+                NAME_CONFIG,
+                ATTRIBUTES_CONFIG,
+                RESOURCE_CONFIG,
+                TRACE_FLAGS_CONFIG,
+                TRACE_ID_CONFIG,
+                SPAN_ID_CONFIG,
+                SEVERITY_TEXT_CONFIG
+        ));
     }
 
     @Override
